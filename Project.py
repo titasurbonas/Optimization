fc265216dcf499f1153e4650f35c72332f68ba54
Titas <titasurbonas@gmai.com>
2018-11-22 18:56:52 +0100

    added NearestNeighborsClassifier

--- Project.py
+++ Project.py
@@ -1,58 +1,74 @@
 #!/usr/bin/env python
 
 import pandas as pd
-from matplotlib import pyplot as plt
-data = pd.read_csv('./ORL_txt/orl_data.txt' ,sep="	", header=None)
-data.drop(data.columns[400], axis=1, inplace=True)
-print(data)
-plt.plot(data)
-plt.show()
-############################################################
-# Fit a PCA
-from sklearn.decomposition import PCA
-pca = PCA(n_components=2, whiten=True)
-pca.fit(data)
-# Project the data in 2D
-X_pca = pca.transform(data)
+from sklearn.model_selection import train_test_split
 
-# Visualize the data
-#target_ids = range(len(data.target_names))
+###########################################################
+# Read the data fromm files 
+
+orl_data = pd.read_csv('./ORL_txt/orl_data.txt' ,sep="	", header=None).transpose()
+orl_data.drop(orl_data.index[400], axis=0, inplace=True)
+orl_data_labels = pd.read_csv('./ORL_txt/orl_lbls.txt ', header=None)
+#print (orl_data_labels)
+#print (orl_data)
+
+MNIST_train_data = pd.read_csv('./MNIST/mnist_train.csv', header=None)
+MNIST_train_labels = MNIST_train_data.iloc[:,0]
+MNIST_train_data.drop(MNIST_train_data.index[0], axis=1, inplace=True)
 
 
-plt.figure(figsize=(10, 10))
-plt.plot(X_pca)
-plt.show()
-"""
-for i, c, label in zip(target_ids, 'rgbcmykw', data.target_names):
-    plt.scatter(X_pca[y == i, 0], X_pca[y == i, 1],c=c, label=label)
-plt.legend()
-plt.show()
-"""
-"""
-from sklearn import datasets
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+MNIST_test_data = pd.read_csv('./MNIST/mnist_test.csv', header=None)
+MNIST_test_labels = MNIST_test_data.iloc[:,0]
+MNIST_test_data.drop(MNIST_test_data.index[0], axis=1, inplace=True)
+
+	
+
+#print(MNIST_train_data)
 
 ############################################################
 # Fit a PCA
-from sklearn.decomposition import PCA
-from matplotlib import pyplot as plt
-pca = PCA(n_components=4, whiten=True)
-pca.fit(X)
+def applyPCA(data):
+	from sklearn.decomposition import PCA
+	from sklearn.preprocessing import StandardScaler
+	f_transform = StandardScaler().fit_transform(data)
+	pca = PCA(n_components=2, whiten=True).fit_transform(data)
+	#pca.fit(data)
+	# Project the data in 2D
+	#pca.transform(data)
+	#print(pca)
+	return pca
+def displayData(data_2D, labels):	
+	from matplotlib import pyplot as plt
+	plt.figure(figsize=(10, 10))
+	plt.scatter(data_2D[:,0], data_2D[:,1]) 
+	plt.show()
+def NearestCentroidClassifier(data, label):
+	from sklearn.neighbors.nearest_centroid import NearestCentroid
+	clf = NearestCentroid().fit(data, label)
+	return clf
+def  NearestNeighborsClassifier(data, label):
+	from sklearn.neighbors import KNeighborsClassifier
+	nnc = KNeighborsClassifier(n_neighbors=3)
+	nnc.fit(data,label)
+	return nnc
 
-############################################################
-# Project the data in 2D
-X_pca = pca.transform(X)
 
-############################################################
-# Visualize the data
-target_ids = range(len(iris.target_names))
+MNIST_2D_train = applyPCA(MNIST_train_data)
+MNIST_2D_test = applyPCA(MNIST_test_data)
 
-from matplotlib import pyplot as plt
-plt.figure(figsize=(6, 5))
-for i, c, label in zip(target_ids, 'rgbcmykw', iris.target_names):
-    plt.scatter(X_pca[y == i, 0], X_pca[y == i, 1],c=c, label=label)
-plt.legend()
-plt.show()
+orl_data_2D = applyPCA(orl_data)
+orlD_Train , orlD_Test , orlL_Train, orlL_Test  = train_test_split(orl_data, orl_data_labels, test_size=0.3, random_state=0)
+classifierORL = NearestNeighborsClassifier(orlD_Train,orlL_Train)
+print (classifierORL.score(orlD_Test,orlL_Test))
 """
+print("hear")
+classifierMNIST = NearestNeighborsClassifier(MNIST_train_data,MNIST_train_labels)
+print (classifierMNIST.score(MNIST_test_data,MNIST_test_labels))
+"""
+#nccc_orl = NearestCentroidclassifier(orlD_Train,orlL_Train)
+#print (nccc_orl.score(orlD_Test,orlL_Test))
+
+#displayData(orl_data_2D)
+#
+#MNIST_train_data_2D = applyPCA(MNIST_train_data)
+#displayData(MNIST_train_data_2D)
